d2fd233dc6370c30564fec9a1e9ee2e1718149e1
JosephGolubchik <37150623+JosephGolubchik@users.noreply.github.com>
2018-05-23 17:42:29 +0300

    Update Champion.h

--- Champion.h
+++ Champion.h
@@ -5,7 +5,7 @@ class Champion: public Player {
 public:
 	bool firstMove = false;
 
-	const string name() const override { return "Dani"; }
+	const string name() const override { return "Champion"; }
 	const Coordinate play(const Board& board) override;
 };
 
